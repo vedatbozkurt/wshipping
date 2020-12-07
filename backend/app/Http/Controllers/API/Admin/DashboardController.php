b92266b5cd539c2a412c33c38c9eb2a94f692d75
Vedat Bozkurt <info@wedat.org>
2020-12-08 00:51:15 +0300

    initial setup and passport

---
+++ backend/app/Http/Controllers/API/Admin/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+
+class DashboardController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::find($id);
+        return response()->json($user);
+    }
+}
